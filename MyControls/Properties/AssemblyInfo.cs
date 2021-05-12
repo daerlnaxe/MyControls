ed251f857a13caaa36e4be9eff430aaa552fcaeb
CODOUL Alexandre <axe.test1@gmail.com>
2021-05-12 18:39:03 +0200

    Moved from GDrive to Github

--- MyControls/Properties/AssemblyInfo.cs
+++ MyControls/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MyControls    ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alexandre CODOUL aka Daerlnaxe, Axenn")]
 [assembly: AssemblyProduct("MyControls")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
